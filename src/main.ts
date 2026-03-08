f875ea01816f8e4de2a39c4dd2a13085ea1df8e3
Mostafa Elshikh <youremail@example.com>
2026-03-08 14:20:23 -0700

    Fix dependency conflict and update class-validator

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api'); 
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+  origin: ['http://localhost:4200', 'https://carco-production.up.railway.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
